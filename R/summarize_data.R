e782d28be641714697e47109a401d7d352fe32c1
bgulbis <brian.gulbis@gmail.com>
2016-03-10 08:54:21 -0600

    updated culumlative dose calculation

--- R/summarize_data.R
+++ R/summarize_data.R
@@ -153,8 +153,10 @@ summarize_cont_meds <- function(cont.data, units = "hours") {
     nz.rate <- dplyr::summarize_(nz.rate, .dots = setNames(dots, nm))
 
     # get first and max rates and AUC
-    dots <- list(~sum(med.rate, na.rm = TRUE), ~dplyr::first(med.rate),
-                 ~max(med.rate, na.rm = TRUE), ~MESS::auc(run.time, med.rate),
+    dots <- list(~sum(med.rate * duration, na.rm = TRUE),
+                 ~dplyr::first(med.rate),
+                 ~max(med.rate, na.rm = TRUE),
+                 ~MESS::auc(run.time, med.rate),
                  ~dplyr::last(run.time))
     nm <- c("cum.dose", "first.rate", "max.rate", "auc", "duration")
     summary.data <- dplyr::summarize_(cont.data, .dots = setNames(dots, nm))
