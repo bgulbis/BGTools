08940d8d75da813a9d860fc7e29177f20e337dd6
bgulbis <brian.gulbis@gmail.com>
2016-03-29 09:28:06 -0500

    add functions to save and read RDS files

--- R/read_data.R
+++ R/read_data.R
@@ -396,3 +396,56 @@ assign_blood_prod <- function(event) {
     # vector
     purrr::map_chr(event, get_prod)
 }
+
+#' Read in RDS files
+#'
+#' \code{read_rds} reads in all RDS files from a directory
+#'
+#' This function reads in all RDS files in a given directory and saves them as
+#' objects in the Global Environment.
+#'
+#' @param data.dir A character string with the name of the directory containing
+#'   the RDS files
+#' @param file.ext An optional character string, defaults to .Rds
+#'
+#' @return An \code{R} object
+#'
+#' @seealso \code{\link{readRDS}}
+#'
+#' @export
+read_rds <- function(data.dir, file.ext = ".Rds") {
+    # if (!exists(var.name)) {
+    #     readRDS(paste(data.dir, file.name, sep = "/"))
+    # } else {
+    #     get(var.name)
+    # }
+    raw <- list.files(data.dir, file.ext, full.names = TRUE)
+    nm <- list.files(analysis.dir, file.ext)
+    nm <- stringr::str_replace_all(nm, file.ext, "")
+    files <- purrr::map(raw, readRDS)
+    names(files) <- nm
+    list2env(files, .GlobalEnv)
+}
+
+#' Save data in RDS files
+#'
+#' \code{save_rds} saves data in RDS files
+#'
+#' This function saves all designated data frames in RDS files within a given
+#' directory.
+#'
+#' @param data.dir A character string with the name of the directory containing
+#'   the RDS files
+#' @param pattern A regular expression indicating the objects to be saved
+#' @param file.ext An optional character string, defaults to .Rds
+#'
+#' @return An \code{R} object
+#'
+#' @seealso \code{\link{saveRDS}}
+#'
+#' @export
+save_rds <- function(data.dir, pattern, file.ext = ".Rds") {
+    to.save <- ls(.GlobalEnv, pattern = pattern)
+    purrr::walk(to.save, ~ saveRDS(get(.x), paste0(data.dir, "/", .x, file.ext)))
+}
+
